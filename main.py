68ae24bfaa8c07bee93b6cfa4cdfb68d02a7878e
Kyla Daphnie Varde <ddaphnje@gmail.com>
2026-02-09 17:33:02 +0800

    Added section info for Varde.

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 print("Lab 1: Version Control")
-print("Author: [KYLA DAPHNIE VARDE]")
+print("Author: KYLA DAPHNIE VARDE")
 print("Student ID: TUPM-25-0753")
+print("Lab 1: Version Control")
+print("Author: Kyla Daphnie Varde")
+print("Student ID: TUPM-25-0753")
+print("Class Section: BSME 1B")
+
